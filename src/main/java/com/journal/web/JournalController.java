251fcd8f63bc2da47460a655ef2a8a4bbbca1593
Rosengren <kev.rosengren@gmail.com>
2018-07-08 08:46:59 -0700

    Convert journal controller into a REST controller

--- src/main/java/com/journal/web/JournalController.java
+++ src/main/java/com/journal/web/JournalController.java
@@ -3,17 +3,26 @@ package com.journal.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import org.springframework.web.servlet.ModelAndView;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import com.journal.domain.JournalEntry;
 import com.journal.repository.JournalRepository;
 
-@Controller
+@RestController
 public class JournalController {
+
+  private static final String VIEW_INDEX = "index";
 
   @Autowired
   JournalRepository repo;
@@ -26,9 +35,13 @@ public class JournalController {
     return repo.findAll();
   }
 
-  @RequestMapping("/")
-  public String index(Model model) {
-    model.addAttribute("journal", repo.findAll());
-    return "index";
+  @RequestMapping(
+    value = "/",
+    method = RequestMethod.GET
+  )
+  public ModelAndView index(ModelAndView modelAndView) {
+    modelAndView.setViewName(VIEW_INDEX);
+    modelAndView.addObject("journal", repo.findAll());
+    return modelAndView;
   }
 }
